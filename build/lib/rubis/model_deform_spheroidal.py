be93ed3bb4dbff8e9e985383044936c254ee6161
gautam-404 <gautam.anuj.1195@gmail.com>
2024-12-05 22:01:39 +0530

    Update return values in spheroidal_method to include additional variables

--- build/lib/rubis/model_deform_spheroidal.py
+++ build/lib/rubis/model_deform_spheroidal.py
@@ -811,6 +811,7 @@ def spheroidal_method(*params) :
             zeta, P, rho, phi_eff, rota, 5/3*np.ones_like(zeta)
         )
     # return zeta, r, map_n, rho, phi_g_l, dphi_g_l, eval_w, phi_eff, dphi_eff, P
+    return (N, M, mass, radius, rotation_target, G, map_n, additional_var, zeta, P, rho, phi_eff, rota)
     
         
     
